01db5a4e7e5ec4a56957a8d652544f59cf9f8f3c
lambdaR <lambdaR@users.noreply.github.com>
2022-04-21 23:11:40 +0000

    Push to M3O-Cli repo

--- examples/contact/update/updateAContact.sh
+++ examples/contact/update/updateAContact.sh
@@ -7,6 +7,20 @@ m3o contact update --emails='[
     "address": "work@example.com",
     "label": "work"
   }
+]' --links='[
+  {
+    "label": "blog",
+    "url": "https://blog.joe.me"
+  }
+]' --birthday="1995-01-01" --id="42e48a3c-6221-11ec-96d2-acde48001122" --phones='[
+  {
+    "label": "home",
+    "number": "010-12345678"
+  },
+  {
+    "label": "work",
+    "number": "010-87654321"
+  }
 ]' --social_medias='[
   {
     "label": "twitter",
@@ -16,21 +30,7 @@ m3o contact update --emails='[
     "label": "facebook",
     "username": "joe-facebook"
   }
-]' --note="this person is very important" --id="42e48a3c-6221-11ec-96d2-acde48001122" --name="joe" --phones='[
-  {
-    "label": "home",
-    "number": "010-12345678"
-  },
-  {
-    "label": "work",
-    "number": "010-87654321"
-  }
-]' --links='[
-  {
-    "label": "blog",
-    "url": "https://blog.joe.me"
-  }
-]' --birthday="1995-01-01" --addresses='[
+]' --note="this person is very important" --name="joe" --addresses='[
   {
     "label": "company address",
     "location": "123 street address"
